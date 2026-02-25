30bfcb25e43b33e3f607c040268a026888ad40cf
JustinD-T <justindomagalatang@hotmail.com>
2026-02-25 15:33:01 -0500

    More analysis code

--- Code/Graphing Code/Utils.py
+++ Code/Graphing Code/Utils.py
@@ -9,12 +9,13 @@ def load_data(path):
     df = pd.read_csv(path, comment='#')
 
     headers = [col for col in df.columns if 'Hz' not in col]
-    freqs = [col for col in df.columns if 'Hz' in col]
+    freqs_name = freqs = [col for col in df.columns if 'Hz' in col]
+    freqs = [float(col.replace(" Hz", '').strip()) for col in df.columns if 'Hz' in col]
 
-    spectrum_df = df[freqs]
+    spectrum_df = df[freqs_name]
     headers_df = df[headers]
 
-    return headers_df, spectrum_df
+    return headers_df, spectrum_df, freqs
     
 
     
